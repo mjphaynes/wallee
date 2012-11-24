4b420e308bec3455f6cf5d0bd04056fc1ca44665
Michael Haynes <michael.haynes@gmail.com>
2012-11-24 23:53:28 +0000

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
 
 $(function() {
 
-	var $i = $('input');
+	var $i = $('input'),
+		$form = $('form');
 	
 	$i[0].focus();
 	$i[0].select();
+	
+	function o(o) {
+		$form.stop().animate({opacity: o}, {duration: 300});
+	}
+	
+	$form.hover(
+		function() {o(1);},
+		function() {o(0);}
+	);
+	o(0);
 	
 });
